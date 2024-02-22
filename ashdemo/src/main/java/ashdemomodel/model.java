f177ec9aa27729e611660f2d3c5ced89f9822f6e
apande77 <apande77@LIN51017303.corp.capgemini.com>
2024-02-23 00:51:41 +0530

    modelw

--- ashdemo/src/main/java/ashdemomodel/model.java
+++ ashdemo/src/main/java/ashdemomodel/model.java
@@ -3,8 +3,10 @@ package ashdemomodel;
 public class model {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		  int a =7;
+		  int b =3; 
+		  int c=a % b;
+		System.out.println(c);  
+}
 
 }
