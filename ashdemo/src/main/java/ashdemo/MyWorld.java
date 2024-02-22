e8a9101f54f26cf850ab8b9cf29874c8bdfd3d6e
apande77 <apande77@LIN51017303.corp.capgemini.com>
2024-02-22 22:07:28 +0530

    1ST COMMENT

---
+++ ashdemo/src/main/java/ashdemo/MyWorld.java
@@ -0,0 +1,10 @@
+package ashdemo;
+
+public class MyWorld {
+
+	public static void main(String[] args) {
+		char arr[]={'A','S','H','I','S','H'};  
+		//printing array using for-each loop  
+		for(char i:arr)  
+		System.out.println(i);  
+}}
